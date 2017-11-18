9483cd26f9c0bb6797741a628a04d4aae773104e
Nate McGregor <natecmcgregor@gmail.com>
2017-11-17 18:00:32 -0800

    add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
-  validates :content, :presence => true
+  validates :content, presence: true, length: { minimum: 50, maximum: 250 }
   validates :author, :presence => true
-  validates :rating, :presence => true
+  # validates :rating, :presence => true
+  validates :rating, presence: true, :inclusion => 1..5
 end
