e07d346604254f76081bc842d303e368c857e935
Nate McGregor <natecmcgregor@gmail.com>
2017-11-27 19:23:34 -0800

    write product and review tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Product, 'connected' do
+  it { should have_many :reviews }
+end
+
+describe Product, 'validate' do
+  it { should validate_presence_of :country }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
+end
+
+
+describe Product, 'has_column' do
+  it { should have_db_column(:country).of_type(:string) }
+  it { should have_db_column(:name).of_type(:string) }
+  it { should have_db_column(:cost).of_type(:integer) }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Review, 'connected' do
+  it { should belong_to :product }
+end
+
+describe Review, 'validate' do
+  it { should validate_presence_of :content }
+  it { should validate_length_of(:content).is_at_least(50) }
+  it { should validate_length_of(:content).is_at_most(250) }
+  it { should validate_presence_of :author }
+  it { should validate_presence_of :rating }
+  it { should validate_inclusion_of(:rating).in_range(1..5) }
+end
+
+describe Review, 'has_column' do
+  it { should have_db_column(:content).of_type(:string) }
+  it { should have_db_column(:author).of_type(:string) }
+  it { should have_db_column(:rating).of_type(:integer) }
+  it { should have_db_column(:product_id).of_type(:integer) }
 end
